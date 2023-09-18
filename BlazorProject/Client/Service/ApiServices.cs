652c0ea2202dd6f50c04037ed8018f2c27a98ad7
Ismoil-Nigmatov <ismoilnigmatov98@gmail.com>
2023-09-18 15:30:16 +0500

    Changes

---
+++ BlazorProject/Client/Service/ApiServices.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Json;
+using BlazorProject.Shared;
+
+namespace BlazorProject.Client.Service;
+
+public class ApiServices
+{
+    public HttpClient _httpClient;
+
+    public ApiServices(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Course>> GetCourseList()
+    {
+        var result = await _httpClient.GetFromJsonAsync<List<Course>>("api/Course");
+        return result;
+    }
+
+    public async Task<List<Course>?> GetUserCourses(string email)
+    {
+        var result = await _httpClient.GetFromJsonAsync<List<Course>>("/api/User/course?email=" + email);
+        return result;
+    }
+
+    public async Task<BlazorProject.Shared.Task> GetTask(string taskId)
+    {
+        var result = await _httpClient.GetFromJsonAsync<BlazorProject.Shared.Task>("/api/Task/one?id=" + taskId);
+        return result;
+    }
+
+    public async Task<List<Homework>> GetHomeworkList()
+    {
+        var result = await _httpClient.GetFromJsonAsync<List<BlazorProject.Shared.Homework>>("api/Homework");
+        return result;
+    }
+
+    public async Task<List<Teacher>> GetTeacherList()
+    {
+        var result = await _httpClient.GetFromJsonAsync<List<Teacher>>("api/Teacher");
+        return result;
+    }
+}
---
+++ BlazorProject/Client/Service/NavigationUrls.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorProject.Client.Service
+{
+    public class NavigationUrls
+    {
+        public static Dictionary<string, string> UrlMappings = new Dictionary<string, string>
+        {
+            { "ToCourses", "/allcourses" },
+            { "ToContact", "/contact" },
+            { "ToLogin", "/login" },
+            { "ToReg", "/registration" },
+            { "ToMain", "/" },
+            { "ToUser", "/user/course" },
+            { "ToHomework", "/homework" },
+            {"ToAbout","/aboutus"},
+            {"ToUserIndex", "/user"},
+            {"ToAnswer","/answer"}
+        };
+    }
+}
